7acb71ecf9b4293c3013eb037ee1a1b1d67edff9
k.y <kunyuany@163.com>
2021-05-15 11:47:07 +0800

    Support custom config & write file mode

--- express/start.py
+++ express/start.py
@@ -1,12 +1,9 @@
 from express import EagerModeScene
 from manimlib import *
 
-scene = EagerModeScene()
+scene = EagerModeScene(write_file=True, gif=True)
 
 circle = Circle(radius=0.5, arc_center=[0, 1, 0])
-
-
-
 axes = Axes()
 
 axes.add_coordinate_labels(
@@ -29,8 +26,6 @@ scene.add(axes)
 scene.add(g1)
 scene.play(ShowCreation(g2))
 
-
-
 circle.move_to(axes.c2p(2, 0))
 
 circle.set_fill(BLUE, opacity=0.5)
@@ -38,21 +33,24 @@ circle.set_stroke(BLUE_E, width=4)
 square = Square()
 
 scene.play(ShowCreation(square))
-scene.wait()
-scene.play(ReplacementTransform(square, circle))
-scene.wait()
-
-# Stretched 4 times in the vertical direction
-scene.play(circle.animate.stretch(4, dim=0))
-scene.play(circle.animate.stretch(2, dim=1))
-# Rotate the ellipse 90°
-scene.play(Rotate(circle, TAU / 4))
-# Move 2 units to the right and shrink to 1/4 of the original
-scene.play(circle.animate.shift(2 * RIGHT), circle.animate.scale(0.25))
-# Insert 10 curves into circle for non-linear transformation (no animation will play)
-circle.insert_n_curves(20)
+# scene.wait()
+# # scene.play(ReplacementTransform(square, circle))
+# # scene.wait()
+# #
+# # # Stretched 4 times in the vertical direction
+# # scene.play(circle.animate.stretch(4, dim=0))
+# # scene.play(circle.animate.stretch(2, dim=1))
+# # # Rotate the ellipse 90°
+# # scene.play(Rotate(circle, TAU / 4))
+# # # Move 2 units to the right and shrink to 1/4 of the original
+# # scene.play(circle.animate.shift(2 * RIGHT), circle.animate.scale(0.25))
+# # # Insert 10 curves into circle for non-linear transformation (no animation will play)
+# # circle.insert_n_curves(20)
 # Apply a complex transformation of f(z)=z^2 to all points on the circle
 # scene.play(circle.animate.apply_complex_function(lambda z: 1/(z**2)))
+
+
+# scene.save_end()
 
 scene.hold_on()
 
