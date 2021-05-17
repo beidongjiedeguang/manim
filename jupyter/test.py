12c1192bc02c2ff6c34dd0e87e82f5a110840e68
k.y <kunyuany@163.com>
2021-05-17 13:02:54 +0800

    cleanup

--- jupyter/test.py
+++
@@ -1,8 +0,0 @@
-from express.eager import EagerModeScene, JupyterModeScene
-from manimlib import *
-scene = EagerModeScene()
-circle = Circle()
-circle.move_to([1, 1, 0]).set_color(BLUE)
-
-scene.play(ShowCreation(circle))
-scene.play(FadeOut(circle))
