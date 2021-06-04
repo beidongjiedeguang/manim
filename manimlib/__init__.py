271acea3e30d4bd6e266a1e3c15fc8214a1d7921
k.y <kunyuany@163.com>
2021-06-05 00:25:35 +0800

    Add camera type

--- manimlib/__init__.py
+++ manimlib/__init__.py
@@ -1,6 +1,6 @@
 from manimlib.constants import *
 __name__ = 'manim_kunyuan'
-__version__ = 0.17
+__version__ = 0.18
 from manimlib.animation.animation import *
 from manimlib.animation.composition import *
 from manimlib.animation.creation import *
