d6772b0cc17a79ca3a6fe558707afff3fbf4f8c0
k.y <kunyuany@163.com>
2021-06-15 01:38:22 +0800

    update version

--- manimlib/__init__.py
+++ manimlib/__init__.py
@@ -1,6 +1,6 @@
 from manimlib.constants import *
 __name__ = 'manim_kunyuan'
-__version__ = 0.19
+__version__ = 0.20
 from manimlib.animation.animation import *
 from manimlib.animation.composition import *
 from manimlib.animation.creation import *
