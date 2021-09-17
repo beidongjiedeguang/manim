c9989aab3f25d317a5b7b86a6779e5f7a87b55f5
kunyuan <kunyuany@163.com>
2021-09-17 13:57:30 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from __future__ import print_function
 from setuptools import setup, find_packages
 from glob import glob
 import manimlib as my_package
+from sparrow import yaml_load
+import os
 
+pkgdir="manimlib"
+version_config = yaml_load(os.path.join(pkgdir, "version-config.yaml"))
+name, version = version_config['name'], version_config['version']
 with open(glob('requirements.*')[0], encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 install_requires = [x.strip() for x in all_reqs]
@@ -11,8 +16,8 @@ install_requires = [x.strip() for x in all_reqs]
 with open("README.md", "r", encoding='utf-8') as fr:
     long_description = fr.read()
 
-setup(name=my_package.__name__,
-      version=my_package.__version__,
+setup(name=name,
+      version=version,
       package_data={
           'manimlib': [
               '*.yaml',
